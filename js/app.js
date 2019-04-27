cb2fe169c59d45a30adf0d9fa9295129a331c5b7
SAS0588 <48109100+SAS0588@users.noreply.github.com>
2019-04-26 23:20:56 -0500

    Added functions in App.js
    created functions that will handle user input, generate a computer choice, and the push the choices to the DOM.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,29 @@
+/* // Store the choices
+const choice =  {
+    rock: document.getElementById('rock').innerHTML,
+    paper: document.getElementById('paper').innerHTML,
+    scissors: document.getElementById('scissors').innerHTML
+};
+
+// Set up event listeners
+document.getElementById('rock').addEventListener('click', function(){
+    choiceMade(choice.rock);
+});
+document.getElementById('paper').addEventListener('click', function(){
+    choiceMade(choice.paper);
+});
+document.getElementById('scissors').addEventListener('click', function(){
+    choiceMade(choice.scissors);
+});
+
+// Update view to show the chosen option
+function choiceMade(element){
+    console.log(element);
+} */
+
+/* function choices(x) {
+    console.log(x.id);
+} */
 
 /* Phase 1
 1. Set up event listeners to receive input from choices. (controller)
@@ -5,43 +31,51 @@
 3. Update the view to show the chosen option. (view)
 */
 
-// 1. Set up event listeners
-document.getElementById('rock').addEventListener('click',choiceMade);
-document.getElementById('paper').addEventListener('click',choiceMade);
-document.getElementById('scissors').addEventListener('click',choiceMade);
-
-
-
-
-function choiceMade(){
-    console.log();
+const data = {
+    userChoice: [],
+    computerChoice: []
 };
 
+function choices(x) {
+    data.userChoice[data.userChoice.length] = x.id; // this passes in the id of the html dom element button.
+    generateComputerChoice();
+};
 
+function updateView(){
+    console.log('userChoice: ' + data.userChoice);
+    console.log('computerChoice: ' + data.computerChoice);
+    document.getElementById('userGameDisplay').innerHTML = data.userChoice[data.userChoice.length - 1];
+    document.getElementById('compGameDisplay').innerHTML = data.computerChoice[data.computerChoice.length - 1];
+    previousChoices();
+};
 
-
-var controller = (function (){
-
-    return{
-
+function generateComputerChoice(){
+    let x = Math.floor((Math.random() * 3) + 1);
+    switch(x){
+        case 1:
+            data.computerChoice[data.computerChoice.length] = 'rock';
+            break;
+        case 2:
+            data.computerChoice[data.computerChoice.length] = 'paper';
+            break;
+        case 3:
+            data.computerChoice[data.computerChoice.length] = 'scissors';
+            break;
+        default:
+        document.getElementById('.compGameDisplay').innerHTML = 'Error';
     };
-});
+    updateView();
+    
+};
 
-var model = (function (){
-
-    return{
-
+function previousChoices(){
+    if (data.userChoice.length == 1){
+        document.getElementById('prevUserChoice').innerHTML = 'No previous choice';
+        document.getElementById('prevCompChoice').innerHTML = 'No previous choice';           
+    } else if (data.userChoice.length > 1) {
+        document.getElementById('prevUserChoice').innerHTML = data.userChoice[data.userChoice.length - 2];
+        document.getElementById('prevCompChoice').innerHTML = data.computerChoice[data.computerChoice.length - 2];
     };
-});
+};
 
-var view = (function(){
-
-    const DOM = {
-        choice: '.objectChoice'
-    };
-
-    return {
-
-    };
-}); 
 
